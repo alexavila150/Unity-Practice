8e1d921329c327f7fde7602b92c1d335ec1aab36
alexavila150 <alexadrianavila@gmail.com>
2020-04-26 17:54:26 -0600

    lerping

--- Unity-SkillShare/Assets/Scripts/Player.cs
+++ Unity-SkillShare/Assets/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float playerSpeed = 10;
+    public float directionalSpeed = 20;
+
+    private Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().velocity = Vector3.forward * (1000 * Time.deltaTime);
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var currVector = gameObject.transform.position;
+        var clamp = Mathf.Clamp(currVector.x + moveHorizontal, -2.5f, 2.5f);
+        var nextVector = new Vector3(clamp, currVector.y, currVector.z);
+        
+        transform.position = Vector3.Lerp(currVector, nextVector , directionalSpeed * Time.deltaTime);
+        //Debug.Log("moveHorizontal: " + moveHorizontal);
+        _rigidbody.velocity = Vector3.forward * (playerSpeed * Time.deltaTime);
+        
     }
 }
