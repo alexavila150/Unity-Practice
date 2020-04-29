9064edd2bd5450d36a94184d8d25597b7e17e54e
alexavila150 <alexadrianavila@gmail.com>
2020-04-29 00:29:32 -0600

    camera movement

---
+++ Unity-SkillShare/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    public void LateUpdate()
+    {
+        var position = gameObject.transform.position;
+        var destination = new Vector3(0, position.y, player.transform.position.z - 10);
+        transform.position = Vector3.Lerp(position, destination, Time.deltaTime * 100);
+    }
+}
