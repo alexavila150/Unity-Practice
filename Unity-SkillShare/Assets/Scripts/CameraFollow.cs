82fed12545af7122dd7b8fb195bd616d49aa8848
alexavila150 <alexadrianavila@gmail.com>
2020-04-29 10:39:27 -0600

    camera distance variable

--- Unity-SkillShare/Assets/Scripts/CameraFollow.cs
+++ Unity-SkillShare/Assets/Scripts/CameraFollow.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-
+    public float cameraDistance;
+    
     public void LateUpdate()
     {
         var position = gameObject.transform.position;
-        var destination = new Vector3(0, position.y, player.transform.position.z - 10);
+        var destination = new Vector3(0, position.y, player.transform.position.z - cameraDistance);
         transform.position = Vector3.Lerp(position, destination, Time.deltaTime * 100);
     }
 }
