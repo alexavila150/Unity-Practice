f34ef25d4742d2f0ea2fba8d090f2c51c0b4adfc
alexavila150 <alexadrianavila@gmail.com>
2020-05-18 21:53:39 -0600

    score logic

---
+++ Unity-SkillShare/Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Score : MonoBehaviour
+{
+    public int score;
+    public void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("gap"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+        }
+    }
+}
