abeeba4872ac9065d05ebf7ab3bc62f959008caf
alexavila150 <alexadrianavila@gmail.com>
2020-05-20 23:16:16 -0600

    Obstacle Spawner

---
+++ Unity-SkillShare/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] trianglePrefabs;
+    private Vector3 _lastSpawnedPosition;
+    
+    public void Update()
+    {
+        var distanceToLsp = Vector3.Distance(player.transform.position, _lastSpawnedPosition);
+        if (distanceToLsp < 120)
+        {
+            SpawnTriangles();
+        }
+    }
+
+    void SpawnTriangles()
+    {
+        _lastSpawnedPosition = new Vector3(0, 0, _lastSpawnedPosition.z + 30);
+        Instantiate(trianglePrefabs[0], _lastSpawnedPosition, Quaternion.identity);
+    }
+}
