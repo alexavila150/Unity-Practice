58b23dabc6bdbc6b2916291eda54137a27b89356
alexavila150 <alexadrianavila@gmail.com>
2020-05-22 21:18:50 -0600

    Shader

---
+++ Unity-SkillShare/Assets/Scripts/App_Initialize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App_Initialize : MonoBehaviour
+{
+    private static readonly int Curvature = Shader.PropertyToID("_Curvature");
+    private static readonly int Trimming = Shader.PropertyToID("_Trimming");
+
+    private void Awake()
+    {
+        Shader.SetGlobalFloat(Curvature, 2.0f);
+        Shader.SetGlobalFloat(Trimming, 0.1f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
