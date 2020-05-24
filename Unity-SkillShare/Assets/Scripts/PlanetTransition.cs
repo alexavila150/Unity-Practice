741b564150fd9a6fa41d62f33793f36dc68d5b8b
alexavila150 <alexadrianavila@gmail.com>
2020-05-23 22:46:19 -0600

    Infinite Planet

---
+++ Unity-SkillShare/Assets/Scripts/PlanetTransition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetTransition : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Wait for 1 second
+        StartCoroutine(nameof(Transfer));
+    }
+
+    private IEnumerator Transfer()
+    {
+        yield return new WaitForSeconds(1);
+        
+        //move the planet forward 200 units
+        var parent = gameObject.transform.parent;
+        var planetPosition = parent.position;
+        parent.position = new Vector3(0, 0, planetPosition.z + 400);
+    }
+}
